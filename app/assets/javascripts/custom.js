dcb5b5e4d1b05212f31d0266f05db589eeb39f9c
PlanetEfficacy <jspevack@gmail.com>
2016-12-30 09:27:23 -0700

    Update js

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -3,30 +3,29 @@ $(document).ready(function() {
 });
 
 function getPhotos() {
-  console.log("getting photos")
+  console.log("getting photos");
   $.getJSON("api/v1/photos")
    .then(renderPhotos);
 }
 
 function renderPhotos(data) {
-  console.log("rendering photos")
+  console.log("rendering photos");
   data.forEach(renderPhoto);
 }
 
 function renderPhoto(photo) {
-  var html = `
-        <div class="col s12 m3 l4">
-          <div class="card small">
-            <div class="card-image">
-              <a href="photos/${photo.id}">
-              <img src="${photo.photo.url}">
-              </a>
-              <span class="card-title">${photo.title}</span>
-            </div>
-            <div class="card-content">
-              <p>${photo.caption}</p>
-            </div>
-          </div>
-        </div>`
+  var html = '<div class="col s12 m3 l4">';
+  html += '<div class="card small">';
+  html += '<div class="card-image">';
+  html += '<a href="photos/' + photo.id + '">';
+  html += '<img src="' + photo.photo.url + '">';
+  html += '</a>';
+  html += '<span class="card-title">' + photo.title + '</span>';
+  html += '</div>';
+  html += '<div class="card-content">';
+  html += '<p>' + photo.caption + '</p>';
+  html += '</div>';
+  html += '</div>';
+  html += '</div>';
   $(".past-photos-of-the-day").append(html);
 }
