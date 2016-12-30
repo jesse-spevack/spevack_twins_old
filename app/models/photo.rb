08bc56ba2b170c30ea7505f6fd55246370bdea14
PlanetEfficacy <jspevack@gmail.com>
2016-12-30 10:18:59 -0700

    Add get first photo of the month

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,7 @@
 class Photo < ApplicationRecord
   mount_uploader :photo, PhotoUploader
 
-  def self.unique_months
+  def self.unique_month_years
     Photo.all.map { |photo| photo.date.strftime('%m %Y') }.uniq
   end
 
@@ -9,4 +9,18 @@ class Photo < ApplicationRecord
     Photo.where('extract(year from date) = ? and extract(month from date) = ?',
                 year, month)
   end
+
+  def self.get_first_photo_of_each_month
+    unique_month_years.map { |d| Photo.month_year(year(d), month(d)).first }
+  end
+
+  private
+
+    def self.year(month_year)
+      month_year.split(" ").first
+    end
+
+    def self.month(month_year)
+      month_year.split(" ").last
+    end
 end
--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Photo, type: :model do
     create :photo, date: Time.parse("2016-12-29")
     create :photo, date: Time.parse("2015-1-29")
 
-    expect(Photo.unique_months).to eq(["12 2015", "12 2016", "01 2015"])
+    expect(Photo.unique_month_years).to eq(["12 2015", "12 2016", "01 2015"])
   end
 
   it "returns photos by month and year" do
@@ -16,5 +16,14 @@ RSpec.describe Photo, type: :model do
     create :photo, date: Time.parse("2015-1-29")
 
     expect(Photo.month_year("12", "2015")).to eq([photo_1, photo_2])
+  end
+
+  it "returns the last photo of each month" do
+    photo_1 = create :photo, date: Time.parse("2015-12-29")
+    create :photo, date: Time.parse("2015-12-30")
+    photo_2 = create :photo, date: Time.parse("2015-1-29")
+    photo_3 = create :photo, date: Time.parse("2016-12-29")
+
+    expect(Photo.get_first_photo_of_each_month).to eq([photo_1, photo_2, photo_3])
   end
 end
