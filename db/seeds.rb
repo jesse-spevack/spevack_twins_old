d0f88bc3eb9252c9e71170976485bf7b1df3791c
PlanetEfficacy <jspevack@gmail.com>
2016-12-29 22:29:00 -0700

    Add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,49 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Photo.create( title: "My great photo 1",
+              caption: "My really great photo caption 1",
+              date: Time.parse("2015-12-29"),
+              photo: Rails.root.join("spec/fixtures/dummy.png").open  )
+
+Photo.create( title: "My great photo 2",
+              caption: "My really great photo caption 2",
+              date: Time.parse("2015-12-30"),
+              photo: Rails.root.join("spec/fixtures/dummy.png").open  )
+
+Photo.create( title: "My great photo 3",
+              caption: "My really great photo caption 3",
+              date: Time.parse("2015-12-31"),
+              photo: Rails.root.join("spec/fixtures/dummy.png").open  )
+
+Photo.create( title: "My great photo 4",
+              caption: "My really great photo caption 4",
+              date: Time.parse("2016-1-1"),
+              photo: Rails.root.join("spec/fixtures/dummy.png").open )
+
+Photo.create( title: "My great photo 5",
+              caption: "My really great photo caption 5",
+              date: Time.parse("2016-1-2"),
+              photo: Rails.root.join("spec/fixtures/dummy.png").open  )
+
+Photo.create( title: "My great photo 6",
+              caption: "My really great photo caption 6",
+              date: Time.parse("2016-1-3"),
+              photo: Rails.root.join("spec/fixtures/dummy.png").open  )
+
+Photo.create( title: "My great photo 7",
+              caption: "My really great photo caption 7",
+              date: Time.parse("2016-1-4"),
+              photo: Rails.root.join("spec/fixtures/dummy.png").open  )
+
+Photo.create( title: "My great photo 8",
+              caption: "My really great photo caption 8",
+              date: Time.parse("2016-1-5"),
+              photo: Rails.root.join("spec/fixtures/dummy.png").open  )
+
+Photo.create( title: "My great photo 9",
+              caption: "My really great photo caption 9",
+              date: Time.parse("2016-1-6"),
+              photo: Rails.root.join("spec/fixtures/dummy.png").open  )
+
+Photo.create( title: "My great photo 10",
+              caption: "My really great photo caption 10",
+              date: Time.parse("2016-1-7"),
+              photo: Rails.root.join("spec/fixtures/dummy.png").open  )
