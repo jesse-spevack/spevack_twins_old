df11f81956cdaf40db50c803124f5a8b32211bfc
PlanetEfficacy <jspevack@gmail.com>
2016-12-30 10:00:34 -0700

    Add month_year to Photo

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,12 @@
 class Photo < ApplicationRecord
   mount_uploader :photo, PhotoUploader
+
+  def self.unique_months
+    Photo.all.map { |photo| photo.date.strftime('%m %Y') }.uniq
+  end
+
+  def self.month_year(month, year)
+    Photo.where('extract(year from date) = ? and extract(month from date) = ?',
+                year, month)
+  end
 end
--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Photo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "returns unique month and year" do
+    create :photo, date: Time.parse("2015-12-29")
+    create :photo, date: Time.parse("2016-12-29")
+    create :photo, date: Time.parse("2015-1-29")
+
+    expect(Photo.unique_months).to eq(["12 2015", "12 2016", "01 2015"])
+  end
+
+  it "returns photos by month and year" do
+    photo_1 = create :photo, date: Time.parse("2015-12-29")
+    photo_2 = create :photo, date: Time.parse("2015-12-30")
+    create :photo, date: Time.parse("2016-12-29")
+    create :photo, date: Time.parse("2015-1-29")
+
+    expect(Photo.month_year("12", "2015")).to eq([photo_1, photo_2])
+  end
 end
